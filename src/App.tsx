f0096e7cee56416aff72919851796acaa7f57904
furPedroRibeiro <pedrocodejob@gmail.com>
2024-01-09 14:03:20 -0300

    arrumando footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,30 @@ import './styles/App.css'
 function App() {
   
   return (
-    <div>
       <div className='flex flex-col items-center justify-center mx-6 gap-10 mt-6'>
         <Header></Header>
         <Body></Body>
+          <section className="
+                    flex flex-col items-center justify-center 
+                    h-auto w-full py-10
+                    bg-terciary
+                ">
+            <address
+                className="
+                  flex flex-col items-center justify-center text-center
+                "
+            >
+            <p className='font-medium text-lg'>
+              By: Pedro Ribeiro <br />
+              <strong>pedrocodejob@gmail.com</strong> <br />
+              <a target='_blank' href='https://www.linkedin.com/in/pedro-ribeiro-abaa0b293' className='underline font-bold'>Linkedin</a> <br />
+              <a target='_blank' href='https://www.github.com/furPedroRibeiro' className='underline font-bold'>Github</a> <br />
+              Idade: 17 <br />
+              Peruíbe, SP
+            </p>
+          </address>
+        </section>
       </div>
-      <section className="
-                  flex flex-col items-center justify-center 
-                  h-auto w-full py-10
-                  bg-terciary
-              ">
-        <address
-            className="
-              flex flex-col items-center justify-center text-center
-            "
-        >
-          <p className='font-medium text-lg'>
-            By: Pedro Ribeiro <br />
-            <strong>pedrocodejob@gmail.com</strong> <br />
-            <a target='_blank' href='https://www.linkedin.com/in/pedro-ribeiro-abaa0b293' className='underline font-bold'>Linkedin</a> <br />
-            <a target='_blank' href='https://www.github.com/furPedroRibeiro' className='underline font-bold'>Github</a> <br />
-            Idade: 17 <br />
-            Peruíbe, SP
-          </p>
-        </address>
-      </section>
-    </div>
   )
 }
 
