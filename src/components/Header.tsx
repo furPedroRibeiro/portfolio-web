ba0b7564f5802e401a3cae9e91e81812ba6de640
furPedroRibeiro <pedrocodejob@gmail.com>
2024-01-09 13:38:39 -0300

    arrumando imagens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header(){
         <section aria-label="Cabeçalho sobre mim">
             <div className='flex flex-wrap-reverse items-center justify-center h-auto gap-8 py-12'>
                 <div className="h-36">
-                    <img className='object-cover rounded-full aspect-square' src="../../public/assets/perfil-picture.jpeg" alt="" width={144}/>
+                    <img className='object-cover rounded-full aspect-square' src="https://arslibri.com.br/assets/home/sobre-nos-fotos/pedro.webp" alt="" width={144}/>
                 </div>
                 <div className='flex flex-col gap-1'>
                     <h1 className='font-black text-2xl text-terciary'>Pedro Luis de Alencar Ribeiro</h1>
